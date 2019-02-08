837041962d5c7565df54c7429198dc35290fd72c
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-02-07 22:48:38 -0500

    Added the layout | login and validation to show/hide properly the logout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import { MeComponent } from "../generated/apolloComponents";
 
 type Props = {
   title?: string;
@@ -38,9 +39,19 @@ const Layout: React.FunctionComponent<Props> = ({
           <a>Login</a>
         </Link>{" "}
         |{" "}
-        <Link href="/logout">
-          <a>Logout</a>
-        </Link>
+        <MeComponent>
+          {({ data, loading }) => {
+            if (!data || loading || !data.me) {
+              return null;
+            }
+
+            return (
+              <Link href="/logout">
+                <a>Logout</a>
+              </Link>
+            );
+          }}
+        </MeComponent>
       </nav>
     </header>
     {children}
